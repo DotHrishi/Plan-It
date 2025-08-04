7e2c019351127aa5c10f6e8a12aad6d099a68ab4
Hrishikesh-Kali <Hrishikesh.kali@gmail.com>
2025-08-04 09:33:30 +0530

    .....

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL || "https://plan-it-backend.onrender.com/api/tasks",
+const api= axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/tasks',
 });
 
 export default api;
 
-
-
-//http://localhost:3000/api/tasks
